78c3970a6cda1eb394e7045d7b727d72ac1c5ee8
Shahar Valiano <shahar.valiano@overops.com>
2019-07-14 09:05:58 +0300

    Added pass/fail check in run.sh

--- jvmti/asm-317876/run.sh
+++ jvmti/asm-317876/run.sh
@@ -14,5 +14,16 @@ $JAVA_HOME/bin/java -agentpath:$PWD/agent/build/libagent.so -jar $PWD/java_hello
 # Decompile the test classes.
 # Notice that in the first command output, StackMapTable is present, and in the second one, it isn't.
 #
-$JAVA_HOME/bin/javap -p -l -v -s -c out/java_util_Date_loaded.class | grep StackMapTable
-$JAVA_HOME/bin/javap -p -l -v -s -c out/java_util_Date_redefined.class | grep StackMapTable
+$JAVA_HOME/bin/javap -p -l -v -s -c out/java_util_Date_loaded.class > out/java_util_Date_loaded.class.disasm.txt
+$JAVA_HOME/bin/javap -p -l -v -s -c out/java_util_Date_redefined.class > out/java_util_Date_redefined.class.disasm.txt
+
+set +e
+FOUND_STACKMAP=`grep "StackMapTable: number_of_entries" out/java_util_Date_redefined.class.disasm.txt`
+set -e
+
+if [ -z "$FOUND_STACKMAP" ]; then
+	echo "Test failed: StackMapTable not found in redefined bytes."
+	exit 1
+fi
+
+echo "Test passed."
